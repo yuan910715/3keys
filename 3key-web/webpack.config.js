f9a01d1112f1ff707a01781bc94d68bb63ffd6c3
MonsterYuan <248655019@qq.com>
2024-07-12 11:00:03 +0800

    v6.0

--- 3key-web/webpack.config.js
+++ 3key-web/webpack.config.js
@@ -56,7 +56,7 @@ module.exports = (options = {}) => ({
   },
   devServer: {
     host: '127.0.0.1',
-    port: 8010,
+    port: 8084,
     proxy: {
       '/api/': {
         target: 'http://127.0.0.1:8080',
